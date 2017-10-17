4892e8bf362715af7c50ec48ecb9d9e0d37661a8
Jonas Marco <jonasmarco.ti@gmail.com>
2017-10-16 22:43:58 -0200

    Modo Noturno

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
-    Foto();
+    foto();
+    loadTema();
 });
 
-function Foto() {
+//Foto
+
+function foto() {
     var itens = $(".about-img");
 
     itens.find(".owl-carousel").owlCarousel({
@@ -12,5 +15,49 @@ function Foto() {
         nav: false,
         dots: true,
         items: 1
+    });
+}
+
+// Funções de Cookies
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+
+// Iniciando tema
+
+function loadTema() {
+    if (getCookie('tema') === 'dark') {
+        document.body.classList.toggle('dark');
+    }
+
+    document.getElementById("btn-night-mode").addEventListener("click", function () {
+        document.body.classList.toggle('dark');
+
+        if (document.body.classList.contains('dark')) {
+            setCookie('tema', 'dark', 365);
+        } else {
+            setCookie('tema', 'light', 365);
+        }
     });
 }
